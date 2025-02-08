26ced703c2bb49fc5f994a450f20addc2fd7ef8d
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-08 11:20:24 +0000

    Create DB context

---
+++ API/Entities/BooksContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Entities;
+
+public class BooksContext(DbContextOptions<BooksContext> options) : DbContext(options)
+{
+    public DbSet<Book> Books { get; set; } = null!;
+
+    public DbSet<Author> Authors { get; set; } = null!;
+
+    public DbSet<Publisher> Publishers { get; set; } = null!;
+}
