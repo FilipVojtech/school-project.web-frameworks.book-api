dcc7b40ae22e19d582feeb601d3bc20ad974920e
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-09 16:05:29 +0000

    Create Publisher DTO

---
+++ API/DTO/PublisherDto.cs
@@ -0,0 +1,12 @@
+﻿using API.Entities;
+
+namespace API.DTO;
+
+public class PublisherDto(Publisher publisher)
+{
+    public int Id { get; set; } = publisher.Id;
+
+    public string Name { get; set; } = publisher.Name;
+
+    public string? Url { get; set; } = publisher.Url;
+}
