e335664f3d838b2ac1d8ef053af3655dc4da49f9
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-09 17:51:34 +0000

    Add Publisher Books DTO

---
+++ API/DTO/PublisherBooksDto.cs
@@ -0,0 +1,25 @@
+﻿using API.Entities;
+
+namespace API.DTO;
+
+public class PublisherBooksDto(Publisher publisher)
+{
+    public int Id { get; set; } = publisher.Id;
+
+    public string Name { get; set; } = publisher.Name;
+    
+    public ICollection<BookDto> Books { get; set; } = publisher.Books
+        .Select(b => new BookDto(b))
+        .ToList();
+
+    public class BookDto(Book book)
+    {
+        public int Id { get; set; } = book.Id;
+
+        public string Title { get; set; } = book.Title;
+
+        public string? Isbn { get; set; } = book.Isbn;
+
+        public AuthorDto Author { get; set; } = new AuthorDto(book.Author);
+    }
+}
