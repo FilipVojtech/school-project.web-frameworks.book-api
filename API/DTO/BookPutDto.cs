a2488bb4b888bad78b15208c85c9880d748ddb48
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-09 17:07:57 +0000

    Add Book PUT DTO

---
+++ API/DTO/BookPutDto.cs
@@ -0,0 +1,16 @@
+﻿using API.Entities;
+
+namespace API.DTO;
+
+public class BookPutDto(Book book)
+{
+    public int Id { get; set; } = book.Id;
+
+    public string Title { get; set; } = book.Title;
+
+    public string? Isbn { get; set; } = book.Isbn;
+
+    public int AuthorId { get; set; } = book.Author.Id;
+
+    public int PublisherId { get; set; } = book.Publisher.Id;
+}
