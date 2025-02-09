49fc50761f42acc68f7eeaed09f8b8280a3843fd
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-09 17:26:38 +0000

    Add remarks to Publisher DTO

--- API/DTO/PublisherDto.cs
+++ API/DTO/PublisherDto.cs
@@ -2,6 +2,11 @@
 
 namespace API.DTO;
 
+/// <remarks>
+///     When an expression property is introduced into the <see cref="API.Entities.Publisher"/> entity
+///     and those values are used within the <see cref="API.DTO.PublisherDto"/>,
+///     create a new Publisher PUT DTO without those properties.
+/// </remarks>
 public class PublisherDto(Publisher publisher)
 {
     public int Id { get; set; } = publisher.Id;
