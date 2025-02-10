437448fa982720d54006999a83a85657b930dcdb
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-10 02:56:28 +0000

    Implement IEquatable for Book

--- API/Entities/Book.cs
+++ API/Entities/Book.cs
@@ -6,7 +6,7 @@ namespace API.Entities;
 
 [Table("books")]
 [Index(nameof(Isbn), IsUnique = true)]
-public class Book
+public class Book : IEquatable<Book>
 {
     [Key]
     [Column("id")]
@@ -22,7 +22,40 @@ public class Book
 
     [ForeignKey("author_id")]
     public required Author Author { get; set; }
-    
+
     [ForeignKey("publisher_id")]
     public required Publisher Publisher { get; set; }
+
+    public bool Equals(Book? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Title == other.Title
+               && Isbn == other.Isbn
+               && Author.Id == other.Author.Id
+               && Publisher.Id == other.Publisher.Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((Book)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Title, Isbn, Author, Publisher);
+    }
+
+    public static bool operator ==(Book? left, Book? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Book? left, Book? right)
+    {
+        return !Equals(left, right);
+    }
 }
