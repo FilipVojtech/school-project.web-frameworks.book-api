596ca8085caafa8dd01f98d801bbee8610cb7370
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-02-16 16:02:52 +0000

    Add no-args constructor

--- API/DTO/PublisherDto.cs
+++ API/DTO/PublisherDto.cs
@@ -8,13 +8,25 @@ namespace API.DTO;
 ///     and those values are used within the <see cref="API.DTO.PublisherDto"/>,
 ///     create a new Publisher PUT DTO without those properties.
 /// </remarks>
-public class PublisherDto(Publisher publisher)
+public class PublisherDto
 {
-    public int Id { get; set; } = publisher.Id;
+    public int Id { get; set; }
 
     [Required]
-    public string Name { get; set; } = publisher.Name;
+    public string Name { get; set; }
 
     [Required]
-    public string? Url { get; set; } = publisher.Url;
+    public string? Url { get; set; }
+
+    public PublisherDto()
+    {
+        Name = string.Empty;
+    }
+
+    public PublisherDto(Publisher publisher, string name)
+    {
+        Id = publisher.Id;
+        Name = publisher.Name;
+        Url = publisher.Url;
+    }
 }
